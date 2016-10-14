834f68e04ca19c754179e558ccc5e499ee2b8cd5
Warren Hansen <whansen1@me.com>
2016-10-14 10:59:43 -0700

    docs: Final Version for the Udacity Nano-degree Program
    This is the final version for On The Map. This iOS app lets you login to udacity, see where others are studying and add your own pin and url of something interesting.
    
    Extra features:
    • Facebook Login
    • Circle activity indicator during login and geocoding
    • Disable and fade UI during login and geocoding
    • Url Scrubbing:
    • Check incoming Url - remove blank spaces and add missing http
    • Check outgoing Url for proper formatting
    • Updated Git Commits

--- On The Map/LogOnViewController.swift
+++ On The Map/LogOnViewController.swift
@@ -13,7 +13,7 @@
 //          Url Scrubbing:
 //          Check incomming Url - remove blank spaces and add missing http
 //          Check outgoing Url for proper formatting
-//          Updated Git Commits
+//          Updated Git Commits with more detail
 
 import UIKit
 import Foundation
